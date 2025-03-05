7685be432d55681616a7b223807d301fca038a59
Luuudaa <159477518+Luuudaa@users.noreply.github.com>
2025-03-05 12:14:46 +0200

    lab1

---
+++ lab1/labs1.js
@@ -0,0 +1,11 @@
+// alert('hello')
+// document.getElementById("showName").onclick = function() {
+//     document.getElementById("hl").textContent = "Андрющенко Людмила";
+// };
+
+    document.getElementById("message").innerText = "Hello world!";
+
+document.getElementById("showName").onclick = function() {
+    console.log("Андрющенко Людмила");
+};
+
